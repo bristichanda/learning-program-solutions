71a87e9e5958c5fa1673e5939b7af59cf5812661
bristichanda <bristichanda2003@gmail.com>
2025-06-29 23:42:08 +0530

    week 2 commit

---
+++ DSA/SettingUpJUnitScenario/src/main/java/org/example/SampleTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SampleTest {
+    @Test
+    public void sampleTest() {
+        assertTrue(true);
+    }
+}
+
+
